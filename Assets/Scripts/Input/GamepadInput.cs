f8c763187f5be48bb5b2b58f837f202718eeb58e
Christoph Wedenig <christoph@wedenig.org>
2018-12-14 20:16:00 +0100

    Added (buggy) Movement
    from last year lol

---
+++ Assets/Scripts/Input/GamepadInput.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class GamepadInput : MonoBehaviour
+{
+    private static GamepadInput _instance;
+
+    private static bool EnablePlayerControls = true;
+    private static bool EnableColorControls = true;
+
+    private void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+    }
+
+    // Use this for initialization
+    // ReSharper disable once Unity.RedundantEventFunction
+    void Start()
+    {
+        EnableColorControls = true;
+        EnablePlayerControls = true;
+    }
+
+    // Update is called once per frame
+    // ReSharper disable once Unity.RedundantEventFunction
+    void Update()
+    {
+        //foreach (string s in Input.GetJoystickNames())
+        //    Debug.Log(s);
+
+        //int i = 0;
+        //while (i < 4)
+        //{
+        //    if (Mathf.Abs(Input.GetAxis("Joy" + (i+1) + "X")) > 0.2F || Mathf.Abs(Input.GetAxis("Joy" + (i + 1) + "Y")) > 0.2F)
+        //        Debug.Log(Input.GetJoystickNames()[i] + " is moved");
+
+        //    i++;
+        //}
+        
+    /*
+        if (Jump())
+            Debug.Log("jump pressed");
+
+        if (Dash())
+            Debug.Log("dash pressed");
+
+        if (Color1())
+            Debug.Log("left trigger pressed");
+
+        if (Color2())
+            Debug.Log("right trigger pressed");
+
+        if (ColorMixed())
+            Debug.Log("both trigger pressed");
+        */
+
+    }
+
+    private const string Color1Button = "Color1";
+    private const string Color2Button = "Color2";
+    private const string Horizontal = "Horizontal";
+    private const string HorizontalSwitch = "HorizontalSwitch";
+    private const string PauseAxis = "Pause";
+    private const double Tolerance = .1;
+
+    private static readonly string[] SwitchGamepadNames = { "Wireless Gamepad", "Unknown Pro Controller" };
+
+    private static bool IsNintendoSwitchProController(string controllerName)
+    {
+        return SwitchGamepadNames.Contains(controllerName);
+    }
+    
+    public static float HorizontalVal()
+    {
+        var horVal = Input.GetAxis(Horizontal);
+
+        foreach (var controllerName in Input.GetJoystickNames())
+        {
+            var horValSwitch = Input.GetAxis(HorizontalSwitch);
+
+            if (!IsNintendoSwitchProController(controllerName) || Math.Abs(horValSwitch) < Tolerance) continue;
+            
+            horVal = horValSwitch;
+            break;
+        }
+
+        return EnablePlayerControls ? horVal : 0;
+    }
+
+
+    public static bool Jump()
+    {
+        return EnableColorControls && (Input.GetKeyDown(KeyCode.Space) || // keyboard
+                                       Input.GetKeyDown(KeyCode.JoystickButton0) || // switch and xbox controller
+                                       Input.GetKeyDown(KeyCode.JoystickButton16)); // macOS binding
+    }
+
+    public static bool Dash()
+    {
+        return EnableColorControls && (Input.GetKeyDown(KeyCode.LeftShift) || // keyboard
+                                       Input.GetKeyDown(KeyCode.JoystickButton2) || // switch and xbox controller
+                                       Input.GetKeyDown(KeyCode.JoystickButton18)); // macOS binding
+    }
+
+    public static bool Color1()
+    {
+        return EnableColorControls &&
+               Input.GetAxis(Color1Button) > Tolerance &&
+               Input.GetAxis(Color2Button) < Tolerance;
+    }
+
+    public static bool Color2()
+    {
+        return EnableColorControls &&
+               Input.GetAxis(Color1Button) < Tolerance &&
+               Input.GetAxis(Color2Button) > Tolerance;
+    }
+
+    public static bool ColorMixed()
+    {
+        return EnableColorControls &&
+               Input.GetAxis(Color1Button) > Tolerance &&
+               Input.GetAxis(Color2Button) > Tolerance;
+    }
+
+    public static bool Pause()
+    {
+        return Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton7);
+    }
+}
---
+++ Assets/Scripts/Movement/Player.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Scripts.Movement
+{
+    [RequireComponent (typeof (Controller2D))]
+    public class Player : MonoBehaviour {
+
+        public float jumpHeight = 4;
+        public float moveSpeed = 10;
+        public float timeToJumpApex = .4f;
+        public float dash = 10;
+        float accelerationTimeAirborne = .2f;
+        float accelerationTimeGrounded = .1f;
+
+        private int currentColor;
+
+        public Vector2 wallLeap;
+        
+        public float wallSlideSpeedMax = 3;
+        public float wallStickTime = .25f;
+        float timeToWallUnstick;
+
+        float gravity;
+        float jumpVelocity;
+        Vector3 velocity;
+        float velocityXSmoothing;
+
+        Controller2D controller;
+        //private LevelInit level;
+
+        private bool canDash;
+
+        public Vector3 InitialPosition;
+        public bool InputEnabled;
+        public bool ZeroGravity;
+        public int Deaths;
+
+        void Start() {
+            controller = GetComponent<Controller2D> ();
+            //level = GetComponent<LevelInit>();
+
+            gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+            jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+
+            canDash = true;
+
+            InitialPosition = transform.position;
+            ZeroGravity = false;
+            Deaths = 0;
+        }
+
+        void Update() {
+            Vector2 input = new Vector2 (GamepadInput.HorizontalVal(), 0);
+            int wallDirX = (controller.collisions.left) ? -1 : 1;
+
+            float targetVelocityX = input.x * moveSpeed;
+            if (ZeroGravity)
+            {
+                if (Mathf.Abs(velocity.x) < 0.0001f)
+                {
+                    velocity.x = Mathf.Abs(1 * Mathf.Sign(velocity.x)) < 0.0001f ? 1 : Mathf.Sign(velocity.x);
+                }
+            }
+            else
+            {
+                velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelerationTimeGrounded : accelerationTimeAirborne);
+            }
+
+            bool wallSliding = false;
+            
+            if ((controller.collisions.left || controller.collisions.right) && !controller.collisions.below && !ZeroGravity) {
+                wallSliding = true;
+
+                if (velocity.y < -wallSlideSpeedMax) {
+                    velocity.y = -wallSlideSpeedMax;
+                }
+
+                if (timeToWallUnstick > 0) {
+                    velocityXSmoothing = 0;
+                    velocity.x = 0;
+
+                    if (input.x != wallDirX && input.x != 0) {
+                        timeToWallUnstick -= Time.deltaTime;
+                    }
+                    else {
+                        timeToWallUnstick = wallStickTime;
+                    }
+                }
+                else {
+                    timeToWallUnstick = wallStickTime;
+                }
+
+            }
+
+            // todo check zero gravity
+            if ((controller.collisions.above || controller.collisions.below) /*&& !controller.collisions.zeroGravity*/) {
+                velocity.y = 0;
+            }
+
+            if (controller.collisions.below)
+            {
+                canDash = true;
+            }
+       
+            if (GamepadInput.Jump()) {
+                if (wallSliding) {
+                    velocity.x = -wallDirX * wallLeap.x;
+                    velocity.y = wallLeap.y;
+                }
+                if (controller.collisions.below) {
+                    //GameManager.instance.playJumpSound();
+                    velocity.y = jumpVelocity;
+                }
+
+			
+            }
+
+            if (GamepadInput.Dash() && canDash && input.normalized.x != 0)
+            {
+                //GameManager.instance.playDashSound();
+                velocity.x = input.normalized.x * dash;
+                velocity.y = 7; //very importante
+                canDash = false;
+            }
+        
+            if (!ZeroGravity)
+            {
+                velocity.y += gravity * Time.deltaTime;
+            }
+
+            controller.Move (velocity * Time.deltaTime, input);
+        }
+    }
+}

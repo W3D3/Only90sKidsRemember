916ae9ab3ac8bb47ad5a7fae859f0e8f4abfa934
Alex <allercher@edu.aau.at>
2018-12-15 20:28:49 +0100

    Throwables can turn; Jojo is turning

--- Assets/Scripts/Throwing/JoJoScript.cs
+++ Assets/Scripts/Throwing/JoJoScript.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class JoJoScript : ThrowableScript
 {
-    private bool collisionHappened = false;
-
     public Color LineColor;
 
     // Start is called before the first frame update
@@ -16,22 +14,18 @@ public class JoJoScript : ThrowableScript
     // Update is called once per frame
     void Update()
     {
-        if (collisionHappened)
-            LineRenderer.SetPositions(new Vector3[] { Thrower.transform.position, this.transform.position });
+        LineRenderer.startWidth = 0.1f;
+        LineRenderer.endWidth = 0.1f;
+        LineRenderer.SetPositions(new Vector3[] { Thrower.transform.position, this.transform.position });
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Wall")
         {
-            collisionHappened = true;
-
             RigidBody.velocity = Vector2.zero;
             Thrower.StartSpidermanMove(gameObject);
-
-            LineRenderer.startWidth = 0.1f;
-            LineRenderer.endWidth = 0.1f;
-
+            
             Invoke("DestroyJojo", 0.4f);
         }
     }

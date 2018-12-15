4cfd9dc1a62740e89486216aff8f84330c7b26ef
Christoph Wedenig <christoph@wedenig.org>
2018-12-15 13:17:39 +0100

    Simple Explosion

---
+++ Assets/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float radius = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var affected in Physics2D.OverlapCircleAll(transform.position, radius))
+        {
+            var playerComponent = affected.GetComponent<Player>();
+            if (playerComponent != null)
+            {
+                playerComponent.Damage();
+            }
+        }
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Start();
+    }
+}

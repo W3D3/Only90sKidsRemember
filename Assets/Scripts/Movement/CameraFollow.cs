1331e04472c6c125213cdbff0e18f366aa15ade8
Alex <allercher@edu.aau.at>
2018-12-16 10:42:07 +0100

    follow cam min size and padding

--- Assets/Scripts/Movement/CameraFollow.cs
+++ Assets/Scripts/Movement/CameraFollow.cs
@@ -6,10 +6,22 @@ using System.Linq;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Vector2 focusAreaSize;
+    /// <summary>
+    /// Additional camera padding around the outer players.
+    /// </summary>
+    public Vector2 CameraPadding;
+
+    /// <summary>
+    /// The desired minimal size for the camera.
+    /// </summary>
+    public float MinimalCameraSize;
+
+    /// <summary>
+    /// The offset for the camera center.
+    /// </summary>
     public Vector2 CameraOffset;
 
-    public GameManagerScript gameManager;
+    public GameManagerScript GameManager;
 
     /// <summary>
     /// The middle between all players.
@@ -27,8 +39,6 @@ public class CameraFollow : MonoBehaviour
     /// </summary>
     private float minimalCameraSize;
 
-    private FocusArea focusArea;
-
     void Start()
     {
     }
@@ -42,7 +52,7 @@ public class CameraFollow : MonoBehaviour
         float maxX = float.MinValue, minX = float.MaxValue;
         float maxY = float.MinValue, minY = float.MaxValue;
 
-        var positions = gameManager.AllPlayers.Select(p => p.gameObject.transform.position);
+        var positions = GameManager.AllPlayers.Select(p => p.gameObject.transform.position);
         foreach (Vector3 position in positions)
         {
             if (maxX < position.x)
@@ -55,11 +65,13 @@ public class CameraFollow : MonoBehaviour
                 minY = position.y;
         }
 
-        var deltaX = (maxX - minX);
-        var deltaY = (maxY - minY);
+        var deltaX = maxX - minX + CameraPadding.x * 2;
+        var deltaY = maxY - minY + CameraPadding.y * 2;
 
         cameraDelta = new Vector2(deltaX, deltaY);
-        cameraCenter = new Vector2(minX + deltaX / 2 + CameraOffset.x, minY + deltaY / 2 + CameraOffset.y);
+        cameraCenter = new Vector2(
+            minX + deltaX / 2 - CameraPadding.x + CameraOffset.x, 
+            minY + deltaY / 2 - CameraPadding.y + CameraOffset.y);
     }
 
     private Bounds CameraCenterBounds
@@ -70,83 +82,31 @@ public class CameraFollow : MonoBehaviour
     /// <summary>
     /// Calculates the minimal needed camera size to show all players.
     /// </summary>
-    private void InitCameraSize()                                                                                                                                                                                  
+    private void InitCameraSize()
     {
         // aspect ration = width / height
         var camera = this.GetComponent<Camera>();
 
         float neededX = cameraDelta.x / 2;
-        float neededYFromX = ((float)Math.Pow(camera.aspect, -1)) * neededX; // height / width * width
+        float neededYFromX = ((float) Math.Pow(camera.aspect, -1)) * neededX; // height / width * width
         float neededY = cameraDelta.y / 2;
 
-         minimalCameraSize  = Math.Max(neededY, neededYFromX);
+        var neededMinY = Math.Max(neededY, neededYFromX);
+        minimalCameraSize = Math.Max(MinimalCameraSize, neededMinY);
     }
 
     void LateUpdate()
     {
         InitCameraPosition();
         InitCameraSize();
-        focusArea = new FocusArea(CameraCenterBounds, focusAreaSize);
-        focusArea.Update(CameraCenterBounds);
 
-        GetComponent<Camera>().orthographicSize = minimalCameraSize+1;
+        GetComponent<Camera>().orthographicSize = minimalCameraSize;
         GetComponent<Transform>().position = new Vector3(cameraCenter.x, cameraCenter.y, -10);
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = new Color(1, 0, 0, .5f);
-        Gizmos.DrawCube(focusArea.centre, focusAreaSize);
-    }
-
-    struct FocusArea
-    {
-        public Vector2 centre;
-        public Vector2 velocity;
-        float left, right;
-        float top, bottom;
-
-
-        public FocusArea(Bounds targetBounds, Vector2 size)
-        {
-            left = targetBounds.center.x - size.x / 2;
-            right = targetBounds.center.x + size.x / 2;
-            bottom = targetBounds.min.y;
-            top = targetBounds.min.y + size.y;
-
-            velocity = Vector2.zero;
-            centre = new Vector2((left + right) / 2, (top + bottom) / 2);
-        }
-
-        public void Update(Bounds targetBounds)
-        {
-            float shiftX = 0;
-            if (targetBounds.min.x < left)
-            {
-                shiftX = targetBounds.min.x - left;
-            }
-            else if (targetBounds.max.x > right)
-            {
-                shiftX = targetBounds.max.x - right;
-            }
-
-            left += shiftX;
-            right += shiftX;
-
-            float shiftY = 0;
-            if (targetBounds.min.y < bottom)
-            {
-                shiftY = targetBounds.min.y - bottom;
-            }
-            else if (targetBounds.max.y > top)
-            {
-                shiftY = targetBounds.max.y - top;
-            }
-
-            top += shiftY;
-            bottom += shiftY;
-            centre = new Vector2((left + right) / 2, (top + bottom) / 2);
-            velocity = new Vector2(shiftX, shiftY);
-        }
+        Gizmos.DrawCube(cameraCenter, new Vector2(1, 1));
     }
 }

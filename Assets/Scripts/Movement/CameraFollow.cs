2628100dded3e3ac1351f17ac455faa730343539
Alex <allercher@edu.aau.at>
2018-12-16 12:15:20 +0100

    follow cam

--- Assets/Scripts/Movement/CameraFollow.cs
+++ Assets/Scripts/Movement/CameraFollow.cs
@@ -6,6 +6,9 @@ using System.Linq;
 
 public class CameraFollow : MonoBehaviour
 {
+    public Vector2 FocusAreaSize;
+    private FocusArea focusArea;
+
     /// <summary>
     /// Additional camera padding around the outer players.
     /// </summary>
@@ -38,11 +41,7 @@ public class CameraFollow : MonoBehaviour
     /// The minimal needed camera size to catch all players.
     /// </summary>
     private float minimalCameraSize;
-
-    void Start()
-    {
-    }
-
+    
     /// <summary>
     /// The center and diagonal of all Players, saved in fields for performance.
     /// </summary>
@@ -70,7 +69,7 @@ public class CameraFollow : MonoBehaviour
 
         cameraDelta = new Vector2(deltaX, deltaY);
         cameraCenter = new Vector2(
-            minX + deltaX / 2 - CameraPadding.x + CameraOffset.x, 
+            minX + deltaX / 2 - CameraPadding.x + CameraOffset.x,
             minY + deltaY / 2 - CameraPadding.y + CameraOffset.y);
     }
 
@@ -88,11 +87,17 @@ public class CameraFollow : MonoBehaviour
         var camera = this.GetComponent<Camera>();
 
         float neededX = cameraDelta.x / 2;
-        float neededYFromX = ((float) Math.Pow(camera.aspect, -1)) * neededX; // height / width * width
+        float neededYFromX = ((float)Math.Pow(camera.aspect, -1)) * neededX; // height / width * width
         float neededY = cameraDelta.y / 2;
 
         var neededMinY = Math.Max(neededY, neededYFromX);
         minimalCameraSize = Math.Max(MinimalCameraSize, neededMinY);
+    }
+
+    void Start()
+    {
+        FocusAreaSize = new Vector2(6, 6);
+        focusArea = new FocusArea(CameraCenterBounds, FocusAreaSize);
     }
 
     void LateUpdate()
@@ -100,13 +105,107 @@ public class CameraFollow : MonoBehaviour
         InitCameraPosition();
         InitCameraSize();
 
+//        LateUpdateOld();
+//        return;
+
+        focusArea = new FocusArea(CameraCenterBounds, FocusAreaSize);
+        focusArea.Update(CameraCenterBounds);
+
         GetComponent<Camera>().orthographicSize = minimalCameraSize;
         GetComponent<Transform>().position = new Vector3(cameraCenter.x, cameraCenter.y, -10);
+    }
+
+    Vector2 verticalOffset = new Vector2(0, 0);
+    float lookAheadDirX = 0;
+    private float currentLookAheadX = 0;
+    private float targetLookAheadX = 0;
+    private float smoothLookVelocityX = 1;
+    private float smoothVelocityY = 1;
+    private float lookSmoothTimeX = 1;
+    private float verticalSmoothTime = 1;
+
+    void LateUpdateOld()
+    {
+        focusArea.Update(CameraCenterBounds);
+
+        Vector2 focusPosition = focusArea.centre + Vector2.up * verticalOffset;
+        if (focusArea.velocity.x != 0)
+        {
+            //lookAheadDirX = Mathf.Sign(focusArea.velocity.x);
+            //if (Mathf.Sign(target.playerInput.x) == Mathf.Sign(focusArea.velocity.x) && target.playerInput.x != 0)
+            //{
+            //    lookAheadStopped = false;
+            //    targetLookAheadX = lookAheadDirX * lookAheadDstX;
+            //}
+            //else
+            //{
+            //    if (!lookAheadStopped)
+            //    {
+            //        lookAheadStopped = true;
+            //        targetLookAheadX = currentLookAheadX + (lookAheadDirX * lookAheadDstX - currentLookAheadX) / 4f;
+            //    }
+            //}
+        }
+
+        currentLookAheadX = Mathf.SmoothDamp(currentLookAheadX, targetLookAheadX, ref smoothLookVelocityX, lookSmoothTimeX);
+        focusPosition.y = Mathf.SmoothDamp(transform.position.y, focusPosition.y, ref smoothVelocityY, verticalSmoothTime);
+        focusPosition += Vector2.right * currentLookAheadX;
+        transform.position = (Vector3)focusPosition + Vector3.forward * -10;
     }
 
     void OnDrawGizmos()
     {
         Gizmos.color = new Color(1, 0, 0, .5f);
-        Gizmos.DrawCube(cameraCenter, new Vector2(1, 1));
+        Gizmos.DrawCube(focusArea.centre, FocusAreaSize);
+    }
+
+    struct FocusArea
+    {
+        public Vector2 centre;
+        public Vector2 velocity;
+        float left, right;
+        float top, bottom;
+
+        public FocusArea(Bounds targetBounds, Vector2 size)
+        {
+            left = targetBounds.center.x - size.x / 2;
+            right = targetBounds.center.x + size.x / 2;
+            bottom = targetBounds.min.y;
+            top = targetBounds.min.y + size.y;
+
+            velocity = Vector2.zero;
+            centre = new Vector2((left + right) / 2, (top + bottom) / 2);
+        }
+
+        public void Update(Bounds targetBounds)
+        {
+            float shiftX = 0;
+            if (targetBounds.min.x < left)
+            {
+                shiftX = targetBounds.min.x - left;
+            }
+            else if (targetBounds.max.x > right)
+            {
+                shiftX = targetBounds.max.x - right;
+            }
+
+            left += shiftX;
+            right += shiftX;
+
+            float shiftY = 0;
+            if (targetBounds.min.y < bottom)
+            {
+                shiftY = targetBounds.min.y - bottom;
+            }
+            else if (targetBounds.max.y > top)
+            {
+                shiftY = targetBounds.max.y - top;
+            }
+
+            top += shiftY;
+            bottom += shiftY;
+            centre = new Vector2((left + right) / 2, (top + bottom) / 2);
+            velocity = new Vector2(shiftX, shiftY);
+        }
     }
 }
